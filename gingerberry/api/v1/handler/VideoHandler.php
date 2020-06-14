c383cbbd861d285d884d1cc3a5a7c30463d77531
Ivan Hristov <ivan.hristov@sap.com>
2020-06-14 11:53:53 +0300

    Move frames dir

--- gingerberry/api/v1/handler/VideoHandler.php
+++ gingerberry/api/v1/handler/VideoHandler.php
@@ -98,7 +98,7 @@ class VideoHandler extends Handler
 
         $videoLen = shell_exec("ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 $filePath 2> /dev/null");
         $videoLen = intval($videoLen);
-        $frameFilePath = "tmp_frames/";
+        $frameFilePath = "/tmp/tmp_frames/";
 
         $tsArray = array();
 
